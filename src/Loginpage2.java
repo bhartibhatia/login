03665804604e00c34fa14358c936933c1ff1ff6e
bhartibhatia <38082654+bhartibhatia@users.noreply.github.com>
2018-06-01 16:51:19 +0530

    added files

---
+++ src/Loginpage2.java
@@ -0,0 +1,18 @@
+
+
+
+
+public class Loginpage2 
+{
+
+    
+    public static void main(String[] args) 
+    {
+      login2 l=new login2();
+      l.setVisible(true);
+      l.setSize(1900, 700);
+     
+      
+    }
+    
+}
